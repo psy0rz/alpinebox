849c114c50640844935d55c305bd4454e67503e9
Edwin Eefting <edwin@datux.nl>
2024-03-20 16:40:38 +0100

    github workflow wip

--- devtools/docker/build.sh
+++ devtools/docker/build.sh
@@ -5,6 +5,6 @@ REPO=`cd ../..;pwd`
 #make sure host OS has zfs
 modprobe zfs || true
 
-docker run --rm --privileged -w /repo/devtools -iv $REPO:/repo alpine:3.19 ./createimage.sh
+docker run --rm --privileged -w /repo/devtools -i -v /tmp:/tmp -v $REPO:/repo alpine:3.19 ./createimage.sh
 # docker run --rm --privileged -w /repo/devtools -itv $REPO:/repo alpine:3.19 
 
