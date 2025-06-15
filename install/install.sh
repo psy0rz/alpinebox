038f104444fd279e2071c043716f539bc8d83534
ben-auo <180770701+ben-auo@users.noreply.github.com>
2025-06-15 08:42:29 -0700

    fixes #14

--- install/install.sh
+++ install/install.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
 set -e
 
-DISK=$1
-if ! [ "$DISK" ]; then
-    echo "Usage: $0 <disk>"
-    echo "Will install Alpine Box on your system."
-    echo "Run this from a Alpine installer ISO!"
+DISK=${1:-/dev/$(lsblk -e8 -o NAME,TYPE,SIZE | grep 'disk' | awk '{printf $1;}')}
 
-    echo "Will WIPE existing DATA!"
-    exit 1
+echo "Will install Alpine Box on your system."
+
+read -p "WARNING: installation will overwrite $DISK and all existing data will be lost! Proceed? (y/N) " -n 1 -r
+echo
+if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+    echo "Exiting. No changes have been made to your device."
+    exit
 fi
+
+echo "ALPINEBOX: Writing to disk $DISK ..."
 
 export INSTALL_DISK=$DISK
 export INSTALL_EFI_DEV=$DISK""2
@@ -26,7 +29,7 @@ export INSTALL_ZPOOL=${INSTALL_ZPOOL:-rpool}
 ./6-install-extras.sh 
 ./7-cleanup.sh
 
-eject -s /dev/sr0 || true
+test -e /dev/sr0 && eject -s /dev/sr0 || true
 
 echo "ALPINEBOX: All done, will reboot in 5 seconds.."
 sleep 5
